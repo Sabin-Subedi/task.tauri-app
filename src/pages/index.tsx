46ac2cd3c7bc98316bf127b0f6b3817179083f32
Sabin Subedi <sabinsubedi125@gmail.com>
2022-09-26 20:19:48 +0545

    Initial setup of an app

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+import Image from "next/image";
+import reactLogo from "../assets/react.svg";
+import tauriLogo from "../assets/tauri.svg";
+import nextLogo from "../assets/next.svg";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return <h1>Hello Tauri</h1>;
+}
+
+export default App;
